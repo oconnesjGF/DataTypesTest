bf6ba8aff36878ae8d2482ad2c98aae6f97e0426
oconnesjGF <104014724+oconnesjGF@users.noreply.github.com>
2023-03-21 14:52:42 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var testArr = [];
+var objectArr = [];
+var objectTest = {};
+
+
+testArr[0] =[["Day 0"],["Monday"]];
+testArr[1] =[["Day 1"],["More Monday"]];
+testArr[2] =[["Day 2"],["Still Monday"]];
+testArr[3] =[["Day 3"],["Yes, it's Monday again"]];
+testArr[4] =[["Day 4"],["Finally, Friday"]];
+testArr[5] =[["Day 5"],["Saturday, the day to catch up on personal projects"]];
+testArr[6] =[["Day 6"],["Pre-Monday, preparing for another round"]];
+
+
+objectArr[0] = {
+  "Day 0":"Monday"
+}
+
+objectArr[1] = {
+  "Day 1":"More Monday"
+}
+objectArr[2] = {
+  "Day 2":"Still Monday"
+}
+objectArr[3] = {
+  "Day 3":"Yes, it's Monday again"
+}
+objectArr[4] = {
+  "Day 4":"Finally, Friday"
+}
+objectArr[5] = {
+  "Day 5":"Saturday, the day to catch up on personal projects"
+}
+objectArr[6] = {
+  "Day 6":"Pre-Monday, preparing for another round"
+}
+
+objectTest = {
+  "Day 0":"Monday",
+  "Day 1":"More Monday",
+  "Day 2":"Still Monday",
+  "Day 3":"Yes, it's Monday again",
+  "Day 4":"Finally, Friday",
+  "Day 5":"Saturday, the day to catch up on personal projects",
+  "Day 6":"Pre-Monday, preparing for another round"
+}
+
+console.log(testArr);
+console.log(objectArr);
+console.log(objectTest);
